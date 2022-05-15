9948ea0587962738cbe97282c83a561207099469
MissDine <enidnyatichi1212@gmail.com>
2022-05-15 20:05:16 +0300

    Feat:Edit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+/*import { BrowserRouter as Router, Switch, Route } from "react-router-dom";*/
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 // Components
 import Navbar from "./components/Navbar";
@@ -14,7 +20,7 @@ const App = () => {
       <Navbar />
       <div className="container">
         <Switch>
-          <Route exact path="/" component={HomePage} />
+          <Route exact path="/" element={<HomePage/>} />
           <Route exact path="/about" component={AboutPage} />
           <Route path="/singleshow/:id" component={SinglePage} />
         </Switch>
