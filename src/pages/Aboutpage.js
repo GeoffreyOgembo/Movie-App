16904eee3b274b4360e5297c74858c25edbbce5b
MissDine <enidnyatichi1212@gmail.com>
2022-05-24 09:55:39 +0300

    Feat:edit

--- src/pages/Aboutpage.js
+++ src/pages/Aboutpage.js
@@ -3,7 +3,7 @@ const Aboutpage = () => {
       <div className="about">
         <h3 className="about__title">ABOUT PAGE</h3>
         <div className="about__info">
-          <p>Tv Show Search App (v1.0)</p>
+          <p>Tv Show Search App</p>
           <div>
             <p>Things learnt from app build:</p>
             <ul>
