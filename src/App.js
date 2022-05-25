7c752a10dd83ceeaba572c57611ad7d33fab00f3
MissDine <enidnyatichi1212@gmail.com>
2022-05-25 15:11:09 +0300

    Feat:Edit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /*import React { BrowserRouter as Router, Switch, Route } from "react-router-dom";*/
-import React, {useState, useEffect} from "react";
+import React from "react";
 
 import {
   BrowserRouter as Router,
@@ -12,31 +12,18 @@ import Navbar from "./components/Navbar";
 
 // Pages
 import HomePage from "./pages/Homepage";
-import AboutPage from "./pages/Aboutpage";
+import LoginPage from "./pages/Loginpage";
 import SinglePage from "./pages/Singlepage";
 import FourOhFour from "./pages/404";
-import { collection, getDocs } from "firebase/firestore";
-import {db} from "./firebase-config";
-
-
 
 const App = () => {
-  const [users, setUsers] = useState([])
-  const usersCollectionRef = collection(db, "users")
-  useEffect(() => {
-    const getUsers = async () =>{
-      const data = await getDocs(usersCollectionRef)
-    }
-
-  },[])
   return (
     <Router>
       <Navbar />
       <div className="container">
-      {/* <AboutPage/> */}
         <Routes>
           <Route exact path="/" element={<HomePage/>} />
-          <Route exact path="/about" element={<AboutPage/>} />/
+          <Route exact path="/login" element={<LoginPage/>} />/
           <Route exact path="/singleshow/:id" element={<SinglePage/>} />
           <Route exact path="*" element={<FourOhFour/>} />
         </Routes>
--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React from "react";
 function FourOhFour(){
     return(
         <>
-            <h1>Page not found</h1>
+            <h1>Error!Page not found</h1>
         </>
     )
 }
