636237bc3c17504f9f7e6a56554c2315914a55ad
MissDine <enidnyatichi1212@gmail.com>
2022-05-25 22:06:55 +0300

    Feat:edit

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import HomePage from "./pages/Homepage";
 import LoginPage from "./pages/Loginpage";
 import SinglePage from "./pages/Singlepage";
 import FourOhFour from "./pages/404";
+import ProtectedRoutes from "./ProtectedRoutes";
 
 const App = () => {
   return (
@@ -22,8 +23,9 @@ const App = () => {
       <Navbar />
       <div className="container">
         <Routes>
-          <Route exact path="/" element={<HomePage/>} />
-          <Route exact path="/login" element={<LoginPage/>} />/
+          <Route exact path="/" element={<LoginPage/>} />
+          <Route element = {<ProtectedRoutes/>}/>
+          <Route exact path="/home" element={<HomePage/>} />
           <Route exact path="/singleshow/:id" element={<SinglePage/>} />
           <Route exact path="*" element={<FourOhFour/>} />
         </Routes>
