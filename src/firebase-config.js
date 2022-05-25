d3471ae3b08f670e085f6c3d03b9311609854b47
MissDine <enidnyatichi1212@gmail.com>
2022-05-25 08:40:45 +0300

    Feat:Edit

---
+++ src/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyCfUVWbs_D9XZPjIrlgi6aYIubwmbBBYdI",
+    authDomain: "tv-show-app-c32f6.firebaseapp.com",
+    projectId: "tv-show-app-c32f6",
+    storageBucket: "tv-show-app-c32f6.appspot.com",
+    messagingSenderId: "556685214416",
+    appId: "1:556685214416:web:12889d58a8a86b23a214d5",
+    measurementId: "G-PGY8NN7C76"
+  };
+
+  const app = initializeApp(firebaseConfig)
